8545e65aa1c7e5448d96859727e481cf86361a30
VienB <vienangelobbernales@iskolarngbayan.pup.edu.ph>
2021-12-08 19:58:57 -0800

    second attempt, it's working!

--- Program1.py
+++ Program1.py
@@ -14,21 +14,24 @@ import random
 print("WELCOME PLAYER!")
 Start = "y"
 
-while Start == "y":
-    firstNum = int(input("ENTER YOUR FIRST NUMBER: "))
-    secondNum = int(input("ENTER YOUR SECOND NUMBER: "))
-    thirdNum = int(input("ENTER YOUR THIRD NUMBER: "))
-    randNum1 = random.randint(0,9)
-    randNum2 = random.randint(0,9)
-    randNum3 = random.randint(0,9)
+def Lottery(Start):
+    while Start == "y":
+        firstNum = int(input("ENTER YOUR FIRST NUMBER: "))
+        secondNum = int(input("ENTER YOUR SECOND NUMBER: "))
+        thirdNum = int(input("ENTER YOUR THIRD NUMBER: "))
+        randNum1 = random.randint(0,9)
+        randNum2 = random.randint(0,9)
+        randNum3 = random.randint(0,9)
 
-    if (firstNum and secondNum and thirdNum) in (randNum1 and randNum2 and randNum3):
-        print(f"CONGRATS! YOU ARE A WINNER!")
-    
-    else:
-        print(f"You loss :<")
-        print(f"The winning numbers are: {randNum1}, {randNum2}, {randNum3}.")
-        Start = input("Try again? y/n: ")
+        if firstNum == randNum1 and secondNum == randNum2 and thirdNum == randNum3:
+            print(f"CONGRATS! YOU ARE A WINNER!")
+        
+        else:
+            print(f"You loss :<")
+            print(f"The winning numbers are: {randNum1}, {randNum2}, {randNum3}.")
+            Start = input("Try again? y/n: ")
 
-if Start == "n":
-    print("THANK YOU, BETTER LUCK NEXT TIME!")
+    if Start == "n":
+        print("THANK YOU, BETTER LUCK NEXT TIME!")
+
+Lottery(Start)
